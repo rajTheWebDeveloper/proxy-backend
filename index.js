6a16d069fbc40b2fc997afb0e74b21812f5d5ebc
Muni Raj <webdevmaster.raj@gmail.com>
2024-04-30 12:12:42 +0530

    Ecommerce Proxy

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import env from 'dotenv'
 import ProductRoutes from './routes/productRoutes.js'
 let app=express()
 
+env.config({
+  path: "./config/.env",
+});
 
 app.use(cors())
 app.use(express.json())
 app.use('/api',ProductRoutes)
+
 
 
 
